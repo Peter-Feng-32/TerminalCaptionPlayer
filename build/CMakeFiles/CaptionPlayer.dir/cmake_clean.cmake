50f15d1c28826eec0bf21641913c675f3cc9f38d
Peter Feng <37753741+Coypirus@users.noreply.github.com>
2022-04-01 18:26:08 -0400

    Add json reader

---
+++ build/CMakeFiles/CaptionPlayer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CaptionPlayer.dir/play_captions.cpp.o"
+  "CMakeFiles/CaptionPlayer.dir/play_captions.cpp.o.d"
+  "CaptionPlayer"
+  "CaptionPlayer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CaptionPlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
